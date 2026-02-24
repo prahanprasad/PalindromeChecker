02b6010132f22076f8dcdb191f1f7168a6efc462
prhanaprasad <prahanprasadd@gmail.com>
2026-02-24 15:57:47 +0530

    .\.git\

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,25 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
 
-        String input = "noon";
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
         for (char c : input.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
 
         boolean isPalindrome = true;
 
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
